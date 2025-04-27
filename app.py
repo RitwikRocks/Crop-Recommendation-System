6d95cd1cf20913a9895934b890380ee81442572b
RitwikRocks <101278739+RitwikRocks@users.noreply.github.com>
2025-04-27 19:18:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
+import os
+
+
 
 app = Flask(__name__)
 
@@ -53,4 +56,5 @@ def predict():
      # 4) Render the About this Project Page
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Get port from environment, default to 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
